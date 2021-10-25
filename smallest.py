7473b0868e9bbc16a75270027184a9439e544b07
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-25 16:08:35 +0000

    added smallest.py

---
+++ smallest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Oct 2021
+# This program determines the smallest number
+
+import random
+
+
+def smallest(num_list):
+    # This function determines the smallest number out of the list.
+    for num in num_list:
+        if num_list.index(num) == 0:
+            smallest_num = num
+        else:
+            if smallest_num > num:
+                smallest_num = num
+
+    return smallest_num
+
+
+def main():
+    # This function is the main function
+    number_list = []
+
+    # process & output
+    print("Here is a list of random numbers:\n")
+
+    for counter in range(10):
+        random_num = random.randint(1, 100)
+        number_list.append(random_num)
+        print("The random number {0} is: {1}".format(counter + 1, random_num))
+
+    print("\nThe smallest number is {0}".format(smallest(number_list)))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
